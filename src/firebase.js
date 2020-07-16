7d77a0104daaf3ec32117736c9addd0cc387886c
Roy Corella <corellasanchez@gmail.com>
2020-07-15 20:39:49 -0600

    Packages updated

--- src/firebase.js
+++ src/firebase.js
@@ -12,7 +12,7 @@ const firebaseApp = firebase.initializeApp({
   messagingSenderId: "917512446533",
   appId: "1:917512446533:web:4697952b7a099ba622392c"
 });
-
+//firebase.firestore.setLogLevel('error'); // debug 
 
 export const db = firebaseApp.firestore();
 
